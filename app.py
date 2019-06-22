9ea736aee1e9f85d5cb78976d63063babd4b4d7e
kettipk <52113773+kettipk@users.noreply.github.com>
2019-06-22 17:49:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
 	return "For the demo, navigate to the endpoint /"
 
+@app.route('/user')
+def user():
+	return "user page"
+
 if __name__ == "__main__":
 	app.run(debug=True, use_reloader=True)
+
